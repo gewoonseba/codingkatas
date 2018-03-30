8be8c87a56561d87e6c7bcce4bc938c3cc72bde7
sebastian.stoelen <sebastian.stoelen@gmail.com>
2018-03-30 15:22:35 +0200

    FizzBuzz: Refactored test

--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -10,12 +10,25 @@ public class FizzBuzzTest {
     private FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
-    public void fizzBuzzTest() {
+    public void fizzBuzzTest_Number() {
         assertEquals("1", fizzBuzz.doFizzBuzz(1));
         assertEquals("2", fizzBuzz.doFizzBuzz(2));
+    }
+
+    @Test
+    public void fizzBuzzTest_Fizz(){
         assertEquals("Fizz", fizzBuzz.doFizzBuzz(3));
+    }
+
+    @Test
+    public void fizzBuzzTest_Buzz(){
         assertEquals("Buzz", fizzBuzz.doFizzBuzz(5));
+    }
+
+    @Test
+    public void fizzBuzzTest_FizzBuzz(){
         assertEquals("FizzBuzz", fizzBuzz.doFizzBuzz(15));
+
     }
 
 }
