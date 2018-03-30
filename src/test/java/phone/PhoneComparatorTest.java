05ed226b6f427d28cf509a0eaf45fddac56fb2fe
sebastian.stoelen <sebastian.stoelen@gmail.com>
2018-03-30 16:25:47 +0200

    Phone: Added additional test for comparator

--- src/test/java/phone/PhoneComparatorTest.java
+++ src/test/java/phone/PhoneComparatorTest.java
@@ -3,7 +3,10 @@ package phone;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,6 +19,14 @@ public class PhoneComparatorTest {
         assertEquals(1, comparator.compare("qwe", "rt"));
         assertEquals(-1, comparator.compare("qw", "ert"));
         assertEquals(0, comparator.compare("qwe", "rty"));
+    }
+
+    @Test
+    public void sortTest(){
+        List<String> testList = Arrays.asList("a", "bcd", "ef", "");
+        List<String> expected = Arrays.asList("", "a", "ef", "bcd");
+        Collections.sort(testList, comparator);
+        assertEquals(expected,testList);
     }
 
 }
