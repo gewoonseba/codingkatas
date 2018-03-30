3c728a23d707a0dd66f3c0bba759e020af90cbcb
sebastian.stoelen <sebastian.stoelen@gmail.com>
2018-03-30 15:54:14 +0200

    Phone: Failing tests two phone numbers

--- src/test/java/phone/PhoneListCheckerTest.java
+++ src/test/java/phone/PhoneListCheckerTest.java
@@ -16,8 +16,18 @@ public class PhoneListCheckerTest {
     public void isConsistentTest_Consistent(){
         List<String> phoneList = new ArrayList();
         phoneList.add("911");
+        phoneList.add("234");
 
         assertEquals(true, checker.isConsistent(phoneList));
+    }
+
+    @Test
+    public void isConsistentTest_NotConsistent(){
+        List<String> phoneList = new ArrayList();
+        phoneList.add("911");
+        phoneList.add("911");
+
+        assertEquals(false, checker.isConsistent(phoneList));
     }
 
 }
