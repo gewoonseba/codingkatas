0e614492b8667a84da5df51428254bdf263814a0
sebastian.stoelen <sebastian.stoelen@gmail.com>
2018-04-07 19:23:25 +0200

    TextConverter: Added basic unit test

--- textconverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
+++ textconverter/src/test/java/tddmicroexercises/textconvertor/HtmlTextConverterTest.java
@@ -4,10 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class HtmlTextConverterTest {
+
+    private static final String TEST_PATH = "/Users/sebastian.stoelen/Documents/workspace/codingkatas/textconverter/src/main/resources/testinput.txt";
+
     @Test
-    public void foo() {
-        HtmlTextConverter converter = new HtmlTextConverter("foo");
-        assertEquals("fixme", converter.getFilename());
+    public void foo() throws IOException {
+        HtmlTextConverter converter = new HtmlTextConverter(TEST_PATH);
+        assertEquals(TEST_PATH, converter.getFilename());
+        assertEquals("some test input &amp; &lt; &gt; &quot; &quot;<br />", converter.convertToHtml());
     }
 }
